bf0eccab817d34e199875b8a62053b1affbae64d
Ricardos-Jose <ricardojosegusmaodelima@gmail.com>
2026-02-07 17:25:36 -0300

    slide básico

--- js/slide.js
+++ js/slide.js
@@ -2,27 +2,29 @@ export default class Slide {
   constructor(slide, holder) {
     this.slide = document.querySelector(slide);
     this.holder = document.querySelector(holder);
-    this.dist = {
-      finalposition: 0,
-    };
+    this.dist = { finalPosition: 0, startX: 0, movement: 0 };
   }
-  bindEvents(eventoBindado) {
-    this.onStart = this.onStart.bind(this);
-    this.onMove = this.onMove.bind(this);
-    this.onEnd = this.onEnd.bind(this);
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
   }
-
+  updateDist(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
+    return this.dist.finalPosition - this.dist.movement;
+  }
   onStart(event) {
     event.preventDefault();
-    console.log(this);
+    this.dist.startX = event.clientX;
     this.holder.addEventListener('mousemove', this.onMove);
   }
   onMove(event) {
-    console.log(event);
+    const finalPosition = this.updateDist(event.clientX);
+    this.moveSlide(finalPosition);
   }
 
   onEnd() {
     this.holder.removeEventListener('mousemove', this.onMove);
+    this.dist.finalPosition = this.dist.movePosition;
   }
 
   addSlideEvents() {
@@ -33,5 +35,10 @@ export default class Slide {
     this.bindEvents();
     this.addSlideEvents();
     return this;
+  }
+  bindEvents(eventoBindado) {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
   }
 }
